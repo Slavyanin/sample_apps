269067efa221d8b16b0c3116ce570ae226576531
Slavyanin <slav.mammoth@gmail.com>
2011-10-20 19:34:25 +0600

    Second commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Microblogging App. Designed by Ivanov & Hasanov "
     if @title.nil?
       base_title
     else
@@ -11,6 +11,6 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("logo.png", :alt => "Microblogging App", :class => "round")
   end
 end
